52eeb51cf75dcf48f097f21437db3e01bb0b1fd4
malufreitas <31512601+malufreitas@users.noreply.github.com>
2018-04-04 03:56:49 -0300

    Add files via upload

---
+++ MinhaVaga CRUD/src/main/java/com/example/helen/minhavaga_crud/ClienteDAO.java
@@ -0,0 +1,23 @@
+package com.example.helen.minhavaga_crud;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+public class ClienteDAO {
+
+    private final String TABLE_CLIENTES = "Clientes";
+    private DbGateway gw;
+
+    public ClienteDAO(Context ctx){
+        gw = DbGateway.getInstance(ctx);
+    }
+
+    public boolean salvar(String nome, String sexo, String uf, boolean vip){
+        ContentValues cv = new ContentValues();
+        cv.put("Nome", nome);
+        cv.put("Sexo", sexo);
+        cv.put("UF", uf);
+        cv.put("Vip", vip ? 1 : 0);
+        return gw.getDatabase().insert(TABLE_CLIENTES, null, cv) > 0;
+    }
+}
