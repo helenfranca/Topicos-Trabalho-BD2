962f1ed30ce4426d7ef288719384f34294d1c69a
malufreitas <31512601+malufreitas@users.noreply.github.com>
2018-06-08 11:43:39 -0300

    Update Consultas.sql

--- SQL/Consultas.sql
+++ SQL/Consultas.sql
@@ -1,29 +1,50 @@
-SELECT nome, horareserva, horachegada, horasaida
-FROM Reserva
-INNER JOIN Cliente
-ON (Reserva.id_cliente = Cliente.id_cliente)
+/*  join_cliente_reserva */
+Select nome,email,horareserva
+from cliente
+inner join reserva
+on (cliente.id_cliente = reserva.id_cliente) limit 15
 
 
-
-CREATE VIEW Dados_do_Cartao AS
-SELECT nometitular, datavalidade
-FROM Cartao
-
-SELECT * FROM Dados_do_Cartao
-
+/*  FALTA */
+Select nome,cobertura,status,horareserva
+from reserva
+inner join vaga on (vaga.id_tipo = reserva.id_vaga) 
+left join tipo on (vaga.id_vaga = tipo.id_tipo) limit 10 
 
 
-CREATE VIEW Maiores_Valores AS
-SELECT valor
-FROM Pagamento
-WHERE (valor > CAST(22.00 AS money)) AND (pago IS TRUE)
+/*  view_cliente */
+create view nome_clientes as
+select nome, cpf from cliente;
 
-SELECT * FROM Maiores_Valores
-
+select * from nome_clientes limit 10
 
 
-SELECT EXISTS (
-SELECT status AS Vagas_disponivel
-FROM Vaga
-WHERE status IS TRUE
-)
+/*  view_estacionamento */
+create view estacionamentos as 
+select nome, valor_hora, capacidade from estacionamento;
+
+select * from estacionamentos
+
+
+/*  exists_1 */
+select exists (select * from pagamento where valor > '600,00' and valor < '1000,00')
+
+/*  exists_2 */
+select exists (select distinct pago from pagamento where valor > '600,00' and valor < '1100,00')
+
+/*  exists_3 */
+select not exists (select * from reserva where datareserva > '01-05-2018')
+
+
+/*  check_pagamento */
+alter table pagamento add check (formapagamento = 'Dinheiro' or formapagamento = 'Cartao')
+
+INSERT INTO pagamento (id_pagamento,dataPagamento,valor,pago,formaPagamento,id_cliente) VALUES
+	(501,'2018-06-05','20.00',True,'Cheque',1)
+
+
+/*  check_tipo */
+alter table tipo add check (nome in ('Comum','Deficiente','Idoso','Moto'))
+
+INSERT INTO tipo (id_tipo, nome) VALUES 
+	(5,'Aviao')
