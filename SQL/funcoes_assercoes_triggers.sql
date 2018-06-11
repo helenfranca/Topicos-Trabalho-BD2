8c8d71bb0cd29d6cde5acd9bff4c4fffb6db4417
malufreitas <31512601+malufreitas@users.noreply.github.com>
2018-06-11 02:38:59 +0000

    Update funcoes_assercoes_triggers.sql

--- SQL/funcoes_assercoes_triggers.sql
+++ SQL/funcoes_assercoes_triggers.sql
@@ -28,7 +28,7 @@ CREATE ASSERTION tem_vaga_disponivel CHECK
 
 
 
-
+/* checkPendencias */
 CREATE FUNCTION checkPendencias() RETURNS TRIGGER AS ' 
 BEGIN
 IF EXISTS (Select R.id_cliente, R.id_pagamento, P.id_pagamento from RESERVA R inner join PAGAMENTO P on (P.pago=False)
@@ -46,4 +46,20 @@ Insert into Reserva (id_reserva,dataReserva,horaReserva,horaChegada,dataSaida,ho
 
 
 
+
+/* checkVagaDisponivel */
+CREATE FUNCTION checkVagaDisponivel() RETURNS TRIGGER AS ' 
+BEGIN
+IF EXISTS (Select R.id_vaga, V.id_vaga, V.status from RESERVA R inner join VAGA V on (V.status=False)
+) THEN
+RAISE EXCEPTION ''Erro: Esta vaga já está reservada!''; END IF; RETURN NULL; END
+'
+LANGUAGE plpgsql;
+
+CREATE TRIGGER checkPendenciasTriggercheckVagaDisponivelTrigger AFTER INSERT OR UPDATE OF id_cliente ON RESERVA
+FOR EACH ROW
+EXECUTE PROCEDURE checkPendenciascheckVagaDisponivel();
+
+Insert into Reserva (id_reserva,dataReserva,horaReserva,horaChegada,dataSaida,horaSaida,id_cliente,id_vaga,id_pagamento) VALUES
+(22,'2018-06-08','19:00','19:20','2018-06-08','20:20',2,2,20);
 
